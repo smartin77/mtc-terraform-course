0fc6a69900f9fea86a8f1cd9604827dc1ea1d9e9
smartin77 <svec@vadium.sk>
2025-03-25 16:31:25 +0000

    added files

---
+++ terraform-code/main.tf
@@ -0,0 +1,22 @@
+resource "github_repository" "mtc_repo" {
+  name        = "mtc-repo"
+  description = "Code for MTC"
+  visibility  = "private"
+  auto_init   = true
+}
+
+resource "github_repository_file" "readme" {
+  repository          = github_repository.mtc_repo.name
+  branch              = "main"
+  file                = "README.md"
+  content             = "# This repository is for infra developers"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "index" {
+  repository          = github_repository.mtc_repo.name
+  branch              = "main"
+  file                = "index.html"
+  content             = "Hello Terraform!"
+  overwrite_on_create = true
+}

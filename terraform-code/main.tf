d9ea70326c926ce27cd2264da00ba1f31bcd1cc7
smartin77 <svec@vadium.sk>
2025-03-26 12:19:20 +0000

    terraform count

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,12 +1,19 @@
+resource "random_id" "random" {
+  byte_length = 2
+  count       = 2
+}
+
 resource "github_repository" "mtc_repo" {
-  name        = "mtc-repo"
+  count       = 2
+  name        = "mtc-repo-${random_id.random[count.index].dec}"
   description = "Code for MTC"
   visibility  = "private"
   auto_init   = true
 }
 
 resource "github_repository_file" "readme" {
-  repository          = github_repository.mtc_repo.name
+  count               = 2
+  repository          = github_repository.mtc_repo[count.index].name
   branch              = "main"
   file                = "README.md"
   content             = "# This repository is for infra developers"
@@ -14,9 +21,16 @@ resource "github_repository_file" "readme" {
 }
 
 resource "github_repository_file" "index" {
-  repository          = github_repository.mtc_repo.name
+  count               = 2
+  repository          = github_repository.mtc_repo[count.index].name
   branch              = "main"
   file                = "index.html"
   content             = "Hello Terraform!"
   overwrite_on_create = true
+}
+
+output "repo-names" {
+  value       = github_repository.mtc_repo[*].name
+  description = "Repository Names"
+  sensitive   = true
 }

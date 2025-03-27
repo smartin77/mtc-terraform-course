19073f3f68a0c2073ce34d55d6b5b05b32ca53c9
smartin77 <svec@vadium.sk>
2025-03-27 13:57:14 +0000

    dynamic variables

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,41 +1,36 @@
-resource "random_id" "random" {
-  byte_length = 2
-  count       = var.repo_count
-}
+# resource "random_id" "random" {
+#   byte_length = 2
+#   count       = var.repo_count
+# }
 
 resource "github_repository" "mtc_repo" {
-  count       = var.repo_count
-  name        = "mtc-repo-${random_id.random[count.index].dec}"
-  description = "Code for MTC"
+  for_each    = toset(["dev", "prod"])
+  name        = "mtc-repo-${each.key}"
+  description = "${each.value} Code for MTC"
   visibility  = var.env == "dev" ? "public" : "private"
   auto_init   = true
 }
 
-resource "github_repository_file" "readme" {
-  count               = var.repo_count
-  repository          = github_repository.mtc_repo[count.index].name
-  branch              = "main"
-  file                = "README.md"
-  content             = "# This ${var.env} repository is for infra developers"
-  overwrite_on_create = true
-}
+# resource "github_repository_file" "readme" {
+#   count               = var.repo_count
+#   repository          = github_repository.mtc_repo[count.index].name
+#   branch              = "main"
+#   file                = "README.md"
+#   content             = "# This ${var.env} repository is for infra developers"
+#   overwrite_on_create = true
+# }
 
-resource "github_repository_file" "index" {
-  count               = var.repo_count
-  repository          = github_repository.mtc_repo[count.index].name
-  branch              = "main"
-  file                = "index.html"
-  content             = "Hello Terraform!"
-  overwrite_on_create = true
-}
+# resource "github_repository_file" "index" {
+#   count               = var.repo_count
+#   repository          = github_repository.mtc_repo[count.index].name
+#   branch              = "main"
+#   file                = "index.html"
+#   content             = "Hello Terraform!"
+#   overwrite_on_create = true
+# }
 
-output "clone-urls" {
-  value       = { for i in github_repository.mtc_repo[*] : i.name => i.http_clone_url }
-  description = "Repository Name and URLs"
-  sensitive   = false
-}
-
-# output "varsource" {
-#   value       = var.varsource
-#   description = "Source being used to source variable definition"
+# output "clone-urls" {
+#   value       = { for i in github_repository.mtc_repo[*] : i.name => i.http_clone_url }
+#   description = "Repository Name and URLs"
+#   sensitive   = false
 # }

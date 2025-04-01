5e16f3e908c20ef208f2714e5abca0c153c3760b
smartin77 <svec@vadium.sk>
2025-04-01 11:01:23 +0000

    changes

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -42,7 +42,7 @@ resource "github_repository_file" "index" {
 }
 
 output "clone-urls" {
-  value       = { for i in github_repository.mtc_repo : i.name => i.http_clone_url }
+  value       = { for i in github_repository.mtc_repo : i.name => [i.ssh_clone_url, i.http_clone_url] }
   description = "Repository Name and URLs"
   sensitive   = false
 }

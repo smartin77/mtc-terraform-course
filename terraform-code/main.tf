767627741ddb6f864598f972dd56ac6f83402319
smartin77 <svec@vadium.sk>
2025-04-03 15:49:20 +0000

    dev deploy keys

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -22,6 +22,16 @@ module "repos" {
   repos    = local.repos
 }
 
+module "deploy-key" {
+  for_each = toset(flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"]))
+  source    = "./modules/deploy-key"
+  repo_name = each.key
+}
+
 output "repo-info" {
-  value = { for k,v in module.repos : k => v.clone-urls }
+  value = { for k, v in module.repos : k => v.clone-urls }
+}
+
+output "repo-list" {
+  value = flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"])
 }
---
+++ terraform-code/modules/deploy-key/main.tf
@@ -0,0 +1,25 @@
+variable "repo_name" {
+    description = "Name of the repo that needs a key"
+    type = string
+}
+
+resource "tls_private_key" "this" {
+    algorithm = "ED25519"
+}
+
+resource "github_repository_deploy_key" "this" {
+    title = "${var.repo_name}-key"
+    repository = var.repo_name
+    key = tls_private_key.this.public_key_openssh
+    read_only = false
+}
+
+resource "local_file" "this" {
+    content = tls_private_key.this.public_key_openssh
+    filename = "${path.cwd}/${github_repository_deploy_key.this.title}.pem"
+
+    provisioner "local-exec" {
+        when = destroy
+        command = "rm -f ${self.filename}"
+    }
+}

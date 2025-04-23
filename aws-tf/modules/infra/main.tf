844c72e2f656fbca0925262c57b7b1e91d8d8b23
smartin77 <svec@vadium.sk>
2025-04-23 11:34:04 +0000

    subnets refine

--- aws-tf/modules/infra/main.tf
+++ aws-tf/modules/infra/main.tf
@@ -1,4 +1,8 @@
-# Create a VPC
+locals {
+  azs = data.aws_availability_zones.available.names
+}
+
+# VPC
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
   tags = {
@@ -31,12 +35,22 @@ resource "aws_route" "this" {
   gateway_id             = aws_internet_gateway.this.id
 }
 
-resource "aws_subnet" "this" {
-  for_each   = { for i in range(var.num_subnets) : "public${i}" => i }
-  vpc_id     = aws_vpc.this.id
-  cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
+resource "aws_subnet" "this" {
+  for_each          = { for i in range(var.num_subnets) : "public${i}" => i }
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
+  availability_zone = local.azs[each.value % length(local.azs)]
   tags = {
     Name = "mtc-ecs-${each.key}"
   }
+}
+
+resource "aws_route_table_association" "this" {
+  for_each       = aws_subnet.this
+  subnet_id      = aws_subnet.this[each.key].id
+  route_table_id = aws_route_table.this.id
 }

5c37157daa0ec23150c1c486588eedc68d6f58ac
Martin Švec <svec@vadium.sk>
2025-07-25 20:45:28 +0200

    loadbalancer accessible on port 80

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -36,7 +36,7 @@ module "loadbalancer" {
   lb_unhealthy_threshold = 2
   lb_timeout             = 3
   lb_interval            = 30
-  listener_port          = 8000
+  listener_port          = 80
   listener_protocol      = "HTTP"
 }
 
